099f936b9caae9fbd856fc686239e98059f2898e
Rafael Lins <60063479+rafaelblins@users.noreply.github.com>
2020-03-28 00:29:37 +0000

    Add files via upload

---
+++ For.js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+while (c <=5) {
+    console.log(c)
+    c++
+}
+*/
+
+for (c = 1; c <=5; c++) {           /* inicio; teste; incremento */
+    console.log(c)
+}
+console.log ('FIM!')
