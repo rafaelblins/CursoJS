a7080843c494f61bc3f1240f15fa7d84b4fd0855
Rafael Lins <60063479+rafaelblins@users.noreply.github.com>
2020-03-28 01:15:03 +0000

    Add files via upload

---
+++ Do e While/do-while.js
@@ -0,0 +1,7 @@
+var c = 1
+while (c<=6) {
+    console.log('Ola tudo bem?')
+    c++   /* é o mesmo que c = c + 1*/
+}
+
+
---
+++ Do e While/while.js
@@ -0,0 +1,8 @@
+var c = 1
+do {
+     console.log(`Passo ${c}`)
+    c++   /* é o mesmo que c = c + 1*/
+} while (c<=6) 
+   
+
+
